59bea5399467eb29a09f237e3da62a4ecc96153e
Norbert Logiewa <norbertlogiewa96@gmail.com>
2018-11-06 23:06:39 +0000

    Improve tests for user, use more of Jest

--- routes/api/testUtils.js
+++ routes/api/testUtils.js
@@ -7,17 +7,6 @@ const faker = require('faker');
 const axios = require('axios');
 
 // Project
-const {isOfType, pprint} = require('./../lib');
-
-const PORT = 3000;
-const HOST = '127.0.0.1';
-
-// root URL for REST API
-axios.defaults.baseURL = `http://${HOST}:${PORT}/api`;
-
-// loads axios and includes config from the defaults
-const instance = axios.create();
-
 const winston = require('winston');
 
 /**
@@ -46,6 +35,18 @@ const log = winston.createLogger({
   ],
 });
 
+const PORT = 3000;
+const HOST = '127.0.0.1';
+
+// loads axios and includes config from the defaults
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: `http://${HOST}:${PORT}/api`,
+  headers: {'Content-Type': 'application/json'},
+  responseType: 'json',
+  validateStatus: (status) => true,
+});
+
 /**
  * Starts a child process with the express server running and serving.
  * Requires a reference to the calling object since it modifies `this.serverProcess`.
@@ -72,114 +73,17 @@ function afterAll() {
 }
 
 /**
- * @param {String} s
- * @param {Number} len
+ * @param {{url: String, method: String, [responseType]: String, [data]: Object, [params]: Object}} reqCfg
  * @return {string}
  */
-function truncate(s, len = process.stdout.columns - 5) {
-  return s !== null && s !== undefined ?
-    s.length >= len ? `${s.slice(0, len - 3)} ...` : s :
-    s;
-}
-
-/**
- * @param {String} url
- * @param {String} method
- * @param {Object} [typeSpec]
- * @param {Object} params
- * @return {string}
- */
-function makeMsg(url, method, typeSpec = {}, params = {}) {
-  const testMarker = '[test]';
-  let msg = `${testMarker} ${method.toUpperCase()} ${axios.defaults.baseURL}${url}`;
-  if (typeSpec === null || typeSpec === undefined ||
-    (typeSpec instanceof Object && Object.keys(typeSpec).length === 0)) {
-    return msg;
-  }
-  if (params !== null && params !== undefined && params instanceof
-    Object && Object.keys(params).length > 0) {
-    msg += `\n\n    with ${method.toUpperCase()} params:`;
-    const longestParamLen = Object.keys(params)
-      .reduce((cur, p) => p.length >= cur ? p.length : cur, 0);
-    for (const param in params) {
-      msg += `\n    => ${param.padEnd(longestParamLen)} ${truncate(
-        params[param], 40)}`;
-    }
-  }
-  const longestKeyLen = Object.keys(typeSpec)
-    .reduce((cur, k) => k.length >= cur ? k.length : cur, 0);
-  msg += '\n\n    has properties:\n';
-  for (const pair of Object.entries(typeSpec)) {
-    let [key, val] = pair;
-    val = val.toString();
-    msg += `    => ${key.padEnd(longestKeyLen)} ${truncate(val, 30)}\n`;
-  }
-  return msg;
-}
-
-
-/**
- * Tests a GET request by firing it to url and verifying output against typeSpec.
- *
- * @param {String} url
- * @param {Object} typeSpec
- */
-function testGET(url, typeSpec) {
-  test(makeMsg(url, 'GET', typeSpec), () => {
-    expect.assertions(1);
-    return expect(
-      instance
-        .get(url)
-        .then((res) => {
-          log.debug(`response to GET request to ${url}: ${pprint(res)}`);
-          log.debug(`response data: ${pprint(res.data)}`);
-          return isOfType(res.data, typeSpec);
-        })
-        .catch((err) => {
-          log.warn(
-            `error ${err} occurred due to GET request to ${url}: ${pprint(
-              err)}`);
-          log.warn(`error message: ${err.msgJSON || err.message}`);
-          return false;
-        })).resolves.toBe(true);
-  });
-}
-
-/**
- * Tests a POST request by firing it with postData to url and verifying output against typeSpec.
- *
- * @param {String} url
- * @param {String|Array<String>|Object} typeSpec
- * @param {Object} postData
- */
-function testPOST(url, typeSpec, postData) {
-  test(makeMsg(url, 'POST', typeSpec, postData), () => {
-    expect.assertions(1);
-    return expect(
-      instance
-        .post(url, postData)
-        .then((res) => {
-          log.debug(`response to POST request to ${url}: ${pprint(res)}`);
-          log.debug(`response data: ${pprint(res.data)}`);
-          return isOfType(res.data, typeSpec);
-        })
-        .catch((err) => {
-          log.warn(
-            `error ${err} occurred due to GET request to ${url}: ${pprint(
-              err)}`);
-          log.warn(`error message: ${err.msgJSON || err.message}`);
-          return false;
-        })).resolves.toBe(true);
-  });
-}
-
-/**
- * Tests that suggestions on routes such as `/`, `/user`, `/module` are in a proper format.
- *
- * @param {String|RegExp|Array<String|RegExp>} url
- */
-function testSuggest(url) {
-  return testGET(url, {status: 'String', msg: 'String'});
+function describeTest(reqCfg) {
+  /** @namespace instance.defaults */
+  return `${reqCfg.method.toUpperCase()} ${join(instance.defaults.baseURL,
+    reqCfg.url)}${'params' in reqCfg 
+        ? ' with params: ' + Object.entries(reqCfg.params).map(entry => entry[0] + ': ' + entry[1].slice(0, 10)).join(', ') 
+        : ''}${'data' in reqCfg 
+            ? ' with body ' + Object.entries(entry => entry[0] + ': ' + entry[1].slice(0, 10)) 
+            : ''}`;
 }
 
 /**
@@ -188,34 +92,23 @@ function testSuggest(url) {
  * @return {{email: String, password: String, firstName: String, lastName: String, info: String, isAdmin: Boolean}}
  */
 function randUser() {
-  /** @type {String} */
-  const email = faker.internet.email();
-  /** @type {String} */
-  const password = faker.internet.password();
-  /** @type {String} */
-  const firstName = faker.name.firstName();
-  /** @type {String} */
-  const lastName = faker.name.lastName();
-  /** @type {String} */
-  const info = faker.lorem.text();
   return {
-    email,
-    password,
-    firstName,
-    lastName,
-    info,
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    info: faker.lorem.text(),
     isAdmin: false,
   };
 }
 
 module.exports = {
-  log,
   HOST,
   PORT,
   afterAll,
   beforeAll,
+  describeTest,
+  instance,
+  log,
   randUser,
-  testGET,
-  testPOST,
-  testSuggest,
 };
--- routes/api/user.test.js
+++ routes/api/user.test.js
@@ -1,5 +1,5 @@
 /**
- * Test Suite for `/user/*`.
+ * Test Suite for `/api/user/*`.
  *
  * Plan:
  *
@@ -13,139 +13,388 @@
  * 4. get all info on a user (expect a null value in nullable attrs, non-null email)
  * 5. un-register
  *
+ * NOTE: Carry out updating with credentials in the request body & cookies.
+ *
+ * 6. Check for errors:
+ *
+ * - you cannot un-register twice
+ * - you cannot log in after you un-register
+ * - you cannot GET an un-registered user's info
+ * - you cannot GET an un-registered user's fields
+ * - you cannot update an un-registered user's fields
+ *
+ * NOTE: Check that proper status codes are being sent.
+ * NOTE: Check that the data sent is in correct format & has the properties you expect.
+ *
  * @author Norbert
  */
+
+// Standard Library
+const {spawn} = require('child_process');
+
+// My Code
 const testUtils = require('./testUtils');
-const faker = require('faker');
-const maybe = faker.random.boolean;
-const NO_RUNS = 10;
-const {
-  afterAll: after,
-  beforeAll: before,
-  log,
-  randUser,
-  testGET,
-  testPOST,
-  testSuggest,
-} = testUtils;
+const {instance, log, randUser} = testUtils;
 const {pprint} = require('./../lib');
 
-beforeAll(function() {
-  return before();
+// 3rd Party
+const faker = require('faker');
+const maybe = faker.random.boolean;
+
+const NO_RUNS = 20;
+let serverProcess;
+
+beforeAll(() => {
+  log.info(`testing ${__filename} part of the REST API, starting server`);
+  serverProcess = spawn('npm', ['run', 'start']);
+  const start = new Date().getSeconds();
+  while (Math.abs((new Date().getSeconds() - start)) < 4) {
+    // wait & do nothing
+  }
 });
 
-afterAll(function() {
-  return after();
+afterAll(() => {
+  log.info(`finished testing ${__filename} in REST API, killing server`);
+  serverProcess.kill();
 });
 
-/**
- * Some routes will not yield any data nor will they modify the database but act as directories.
- *
- * If you type `/user` you should get a hint suggesting where you might want to go next (e.g. /user/<email>).
- */
-testSuggest('/user');
-
-log.info(`running user test suite ${NO_RUNS}x`);
-
-const mockUsers = []
+log.info(`running ${__filename} test suite ${NO_RUNS}x`);
 
 for (let i = 0; i < NO_RUNS; i++) {
+  let cookie; // to be set AFTER logging in
+  let user = randUser();
+  let {email, password} = user;
+  let credentials = {email, password};
 
-  const user = randUser();
-  const {email, password} = user;
+  describe(`mock user ${pprint(user)}`, () => {
 
-  log.debug('generated mock user');
-  log.debug(pprint(user));
+    test(`registration of ${email}`, async () => {
+      const res = await instance.request({
+        method: 'post',
+        url: '/user/register',
+        data: credentials,
+      });
+      expect.assertions(4);
+      expect(res.status).toBeLessThanOrEqual(300);
+      expect(res.status).toBeGreaterThanOrEqual(200);
+      expect(res.data).toHaveProperty('msg');
+      expect(res.data).toHaveProperty('status', 'OK');
+    });
 
-  log.info(`testing registration of ${email}`);
-  testPOST(`/user/register`,
-    {status: 'String', msg: 'String'}, {password, email});
+    test(`email of ${email} is not null after registration`,
+      async () => {
+        const res = await instance.request({
+          method: 'get',
+          url: `/user/${email}/email`,
+        });
+        expect.assertions(5);
+        expect(res.status).toBeLessThanOrEqual(300);
+        expect(res.status).toBeGreaterThanOrEqual(200);
+        expect(res.data).toHaveProperty('msg');
+        expect(res.data).toHaveProperty('status', 'OK');
+        expect(res.data).toHaveProperty('result', email);
+      });
 
-  log.info(`testing that email of ${email} is not null after registration`);
-  testGET(`/user/${email}/email`, {
-    status: 'String',
-    msg: 'String',
-    result: 'String',
+    test(`${email} is not an admin by default after registration`,
+      async () => {
+        const res = await instance.request({
+          method: 'get',
+          url: `/user/${email}/isAdmin`,
+        });
+        expect.assertions(5);
+        expect(res.status).toBeLessThanOrEqual(300);
+        expect(res.status).toBeGreaterThanOrEqual(200);
+        expect(res.data).toHaveProperty('msg');
+        expect(res.data).toHaveProperty('status', 'OK');
+        expect(res.data).toHaveProperty('result', false);
+      });
+
+    const nullAttrs = ['firstName', 'lastName', 'info'];
+
+    for (const attr of nullAttrs) {
+      describe(`nullable attribute ${attr}`, () => {
+
+        const value = faker.random.word();
+
+        test(
+          `GET request for ${email}'s ${attr}  yields null after registration`,
+          async () => {
+            const res = await instance.request({
+              method: 'get',
+              url: `/user/${email}/${attr}`,
+            });
+            expect.assertions(5);
+            expect(res.status).toBeLessThanOrEqual(300);
+            expect(res.status).toBeGreaterThanOrEqual(200);
+            expect(res.data).toHaveProperty('msg');
+            expect(res.data).toHaveProperty('status', 'OK');
+            expect(res.data).toHaveProperty('result', null);
+          });
+
+        if (maybe()) {
+
+          test(
+            `POST request to update ${email}'s ${attr} to ${value} with password & value in the request body`,
+            async () => {
+              const res = await instance.request({
+                method: 'post',
+                url: `/user/${email}/${attr}`,
+                data: {password, value},
+              });
+              expect.assertions(4);
+              expect(res.status).toBeLessThanOrEqual(300);
+              expect(res.status).toBeGreaterThanOrEqual(200);
+              expect(res.data).toHaveProperty('msg');
+              expect(res.data).toHaveProperty('status', 'OK');
+            });
+
+        } else {
+
+          test(
+            `POST request to log in in as newly registered ${email} to set ${attr} to ${value}`,
+            async () => {
+              const res = await instance.request({
+                method: 'post',
+                url: `/user/login`,
+                data: credentials,
+              });
+              cookie = res.headers['set-cookie'].join(' ; ');
+              expect.assertions(4);
+              expect(res.status).toBeLessThanOrEqual(300);
+              expect(res.status).toBeGreaterThanOrEqual(200);
+              expect(res.data).toHaveProperty('msg');
+              expect(res.data).toHaveProperty('status', 'OK');
+            });
+
+          test(
+            `POST request to update ${email}'s ${attr} to ${value} after logging in`,
+            async () => {
+              const res = await instance.request({
+                method: 'post',
+                url: `/user/${email}/${attr}`,
+                headers: {cookie},
+                data: {password, value},
+              });
+              expect.assertions(4);
+              expect(res.status).toBeLessThanOrEqual(300);
+              expect(res.status).toBeGreaterThanOrEqual(200);
+              expect(res.data).toHaveProperty('msg');
+              expect(res.data).toHaveProperty('status', 'OK');
+            });
+
+          test(`POST request to log ${email} out after updating their ${attr}`,
+            async () => {
+              const res = await instance.request({
+                method: 'post',
+                headers: {cookie},
+                url: `/user/logout`,
+                withCredentials: true,
+              });
+              cookie = undefined;
+              expect.assertions(4);
+              expect(res.status).toBeLessThanOrEqual(300);
+              expect(res.status).toBeGreaterThanOrEqual(200);
+              expect(res.data).toHaveProperty('msg');
+              expect(res.data).toHaveProperty('status', 'OK');
+            });
+        }
+
+        test(
+          `GET request to access ${email}'s ${attr} yields ${value} after updating`,
+          async () => {
+            const res = await instance.request({
+              method: 'get',
+              url: `/user/${email}/${attr}`,
+            });
+            expect.assertions(5);
+            expect(res.status).toBeLessThanOrEqual(300);
+            expect(res.status).toBeGreaterThanOrEqual(200);
+            expect(res.data).toHaveProperty('msg');
+            expect(res.data).toHaveProperty('status', 'OK');
+            expect(res.data).toHaveProperty('result', value);
+          });
+      });
+    }
   });
 
-  for (const attr of ['firstName', 'lastName', 'info']) {
-
-    log.info(`testing that ${attr} of new user ${email} is null`);
-    testGET(`/user/${email}/${attr}`, {
-      status: 'String',
-      msg: 'String',
-      result: 'null',
+  test(
+    `GET request for all info about ${email} returns all properties except for password`,
+    async () => {
+      expect.assertions(11);
+      const res = await instance.request({
+        method: 'get',
+        url: `/user/${email}`,
+      });
+      expect(res.status).toBeLessThanOrEqual(300);
+      expect(res.status).toBeGreaterThanOrEqual(200);
+      expect(res.data).toHaveProperty('msg');
+      expect(res.data).toHaveProperty('status', 'OK');
+      expect(res.data).toHaveProperty('result');
+      expect(res.data).toHaveProperty('result.email');
+      expect(res.data).toHaveProperty('result.lastName');
+      expect(res.data).not.toHaveProperty('result.password');
+      expect(res.data).toHaveProperty('result.firstName');
+      expect(res.data).toHaveProperty('result.info');
+      expect(res.data).toHaveProperty('result.isAdmin', false);
     });
 
-    const value = user[attr] + 'abc'; // reset to the same
+  test(`GET request for hashed password of ${email} fails`, async () => {
+    const res = await instance.request({
+      method: 'get',
+      url: `/user/${email}/password`,
+    });
+    expect.assertions(5);
+    expect(res.status).toBeLessThanOrEqual(500);
+    expect(res.status).toBeGreaterThanOrEqual(400);
+    expect(res.data).toHaveProperty('msg');
+    expect(res.data).toHaveProperty('status', 'ERROR');
+    expect(res.data).not.toHaveProperty('result');
+  });
 
-    log.info(`testing updating of ${attr} in ${email}`);
-    testPOST(`/user/${email}/${attr}`,
-      {status: 'String', msg: 'String'}, {password, value});
-
-    log.info(`testing getting of ${attr} in ${email}`);
-    testGET(`/user/${email}/${attr}`, {
-      status: 'String',
-      msg: 'String',
-      result: 'String',
+  test(`POST request to set the isAdmin property in ${email} fails`,
+    async () => {
+      const res = await instance.request({
+        method: 'post',
+        url: `/user/${email}/isAdmin`,
+        data: credentials,
+      });
+      expect.assertions(5);
+      expect(res.status).toBeLessThanOrEqual(500);
+      expect(res.status).toBeGreaterThanOrEqual(400);
+      expect(res.data).toHaveProperty('msg');
+      expect(res.data).toHaveProperty('status', 'ERROR');
+      expect(res.data).not.toHaveProperty('result');
     });
 
-    log.info(`testing that ${attr} of ${email} was set to null`);
-    testPOST(`/user/${email}/${attr}`,
-      {status: 'String', msg: 'String'},
-      maybe() ? {email, password, value: null} : {password, value: null},
-    );
+  if (maybe()) {
 
-    log.info(`testing that ${attr} was set to null`);
-    testGET(`/user/${email}/${attr}`, {
-      status: 'String',
-      msg: 'String',
-      result: 'null',
-    });
+    test(
+      `POST request to un-register ${email} with credentials in the request body`,
+      async () => {
+        const res = await instance.request({
+          method: 'post',
+          url: '/user/unregister',
+          data: credentials,
+        });
+        cookie = undefined;
+        expect.assertions(4);
+        expect(res.status).toBeLessThanOrEqual(300);
+        expect(res.status).toBeGreaterThanOrEqual(200);
+        expect(res.data).toHaveProperty('msg');
+        expect(res.data).toHaveProperty('status', 'OK');
+      });
+
+  } else {
+
+    test(
+      `POST request to log in as newly registered ${email} to un-register with credentials in the request body`,
+      async () => {
+        const res = await instance.request({
+          method: 'post',
+          url: `/user/login`,
+          data: credentials,
+        });
+        cookie = res.headers['set-cookie'].join(' ; ');
+        expect.assertions(4);
+        expect(res.status).toBeLessThanOrEqual(300);
+        expect(res.status).toBeGreaterThanOrEqual(200);
+        expect(res.data).toHaveProperty('msg');
+        expect(res.data).toHaveProperty('status', 'OK');
+      });
+
+    test(`POST request to un-register ${email} with credentials in cookies`,
+      async () => {
+        const res = await instance.request({
+          method: 'post',
+          url: '/user/unregister',
+          withCredentials: true,
+          headers: {cookie},
+        });
+        cookie = undefined;
+        expect.assertions(4);
+        expect(res.status).toBeLessThanOrEqual(300);
+        expect(res.status).toBeGreaterThanOrEqual(200);
+        expect(res.data).toHaveProperty('msg');
+        expect(res.data).toHaveProperty('status', 'OK');
+      });
   }
 
-  /**
-   * NOTE there is an issue with booleans being stored as INTS by databases. After all databases are written in C which sees boolean as alias to int.
-   * DO NOT check that is_admin is of type 'Boolean' because it will fail.
-   *
-   * NOTE nullable attrs have been changed to null above.
-   */
-  log.info(`testing getting all info about ${email}`);
-  testGET(`/user/${email}`, {
-    status: 'String',
-    msg: 'String',
-    result: {
-      email: 'String',
-      firstName: 'null',
-      lastName: 'null',
-      info: 'null',
-    },
-  });
+  test(
+    `GET request to access all info about ${email} after they have been un-registered fails`,
+    async () => {
+      const res = await instance.request({
+        method: 'get',
+        url: `/user/${email}`,
+      });
+      expect.assertions(5);
+      expect(res.status).toBeGreaterThanOrEqual(400);
+      expect(res.status).toBeLessThanOrEqual(499);
+      expect(res.data).toHaveProperty('msg');
+      expect(res.data).toHaveProperty('status', 'ERROR');
+      expect(res.data).not.toHaveProperty('result');
+    });
 
-  testGET(`/user/${email}`, {
-    status: 'String',
-    msg: 'String',
-    result: {
-      email: 'String',
-      firstName: 'String?',
-      lastName: 'String?',
-      info: 'String?',
-    },
-  });
+  for (const attr of ['firstName', 'lastName', 'info', 'isAdmin', 'email']) {
+    test(
+      `GET request for ${email}'s ${attr} after their account has been deleted fails`,
+      async () => {
+        const res = await instance.request({
+          method: 'get',
+          url: `/user/${email}/${attr}`,
+        });
+        expect.assertions(5);
+        expect(res.status).toBeGreaterThanOrEqual(400);
+        expect(res.status).toBeLessThanOrEqual(499);
+        expect(res.data).toHaveProperty('msg');
+        expect(res.data).toHaveProperty('status', 'ERROR');
+        expect(res.data).not.toHaveProperty('result');
+      });
 
-  /**
-   * Tests retrieving user's non-nullable attribute.
-   *
-   * NOTE don't check for password because it won't work.
-   */
-  testGET(`/user/${email}/email`,
-    {status: 'String', msg: 'String', result: 'String'});
-}
+    const value = faker.internet.email();
 
+    test(
+      `POST request to update ${email}'s ${attr} to ${value} after their account has been deleted fails`,
+      async () => {
+        const res = await instance.request({
+          method: 'post',
+          url: `/user/${email}/${attr}`,
+          data: {password, value},
+        });
+        expect.assertions(4);
+        expect(res.status).toBeLessThanOrEqual(499);
+        expect(res.status).toBeGreaterThanOrEqual(400);
+        expect(res.data).toHaveProperty('msg');
+        expect(res.data).toHaveProperty('status', 'ERROR');
+      });
+  }
 
-for (const user of mockUsers) {
-  log.info(`testing un-registering of ${user.email}`);
-  testPOST(`/user/unregister`,
-    {status: 'String', msg: 'String'}, {email: user.email, password: user.password},
-  );
+  test(
+    `POST request to un-register ${email} who has already been un-registered fails`,
+    async () => {
+      const res = await instance.request({
+        method: 'post',
+        url: '/user/unregister',
+        data: credentials,
+      });
+      expect.assertions(5);
+      expect(res.status).toBeGreaterThanOrEqual(400);
+      expect(res.status).toBeLessThanOrEqual(499);
+      expect(res.data).toHaveProperty('msg');
+      expect(res.data).toHaveProperty('status', 'ERROR');
+      expect(res.data).not.toHaveProperty('result');
+    });
+
+  test(`POST request to log in in as un-registered ${email} fails`,
+    async () => {
+      const res = await instance.request({
+        method: 'post',
+        url: `/user/login`,
+      });
+      expect.assertions(5);
+      expect(res.status).toBeGreaterThanOrEqual(400);
+      expect(res.status).toBeLessThanOrEqual(499);
+      expect(res.data).toHaveProperty('msg');
+      expect(res.data).toHaveProperty('status', 'ERROR');
+      expect(res.data).not.toHaveProperty('result');
+    });
 }
