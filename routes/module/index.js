1952cad2893cd7a66355513cf95235af7ee4343e
Norbert Logiewa <norbertlogiewa96@gmail.com>
2018-11-28 13:19:42 +0000

    Change module routing to reflect db changes

--- routes/module/index.js
+++ routes/module/index.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const {join} = require('path');
 
 const NoSuchRecord = require('../errors').NoSuchRecord;
-const {Enrollment, Module, Lesson, Rating, sequelize, Sequelize} = require(
-  '../database');
+const {Enrollment, Module, Lesson, Rating, sequelize, User} = require('../database');
 
 /**
  * Search page for all modules.
@@ -15,44 +14,53 @@ router.get('/search', (req, res) => res.render(join('module', 'search')));
 /**
  * Public info about a module.
  */
-router.get('/:name',
+router.get('/:id',
   (req, res, next) => {
-    const {name} = req.params;
-    return Module.findOne({where: {name}})
+    const {id} = req.params;
+    return Module.findOne({where: {id}})
       .then((m) => m === null
-        ? Promise.reject(new NoSuchRecord('Module', {name: req.params.name}))
+        ? Promise.reject(new NoSuchRecord('Module', {id}))
         : m.dataValues)
-      .then((moduleInfo) =>
-        Lesson.findAll({where: {module: moduleInfo.name}})
-          .then((lessons) => {
-            moduleInfo.lessons = lessons.map(l => l.dataValues);
-            return sequelize.query(`SELECT avg(stars) AS average
-                                    from Rating AS r
-                                           INNER JOIN Module AS m
-                                    WHERE r.module = m.name`)
-              .spread((avg, metadata) => {
-                moduleInfo.rating = avg[0].average;
-                return Rating.findAll({
-                  order: sequelize.literal('createdAt DESC'),
-                  limit: 20,
-                  where: {module: moduleInfo.name},
-                }).then((ratings) => {
-                  moduleInfo.ratings = ratings.map(r => r.dataValues);
-                  return res.locals.loggedIn === undefined
-                    ? res.render(join('module', 'index'), {module: moduleInfo})
-                    : Enrollment.findOne({
-                      where: {
-                        module: moduleInfo.name,
-                        student: res.locals.loggedIn.email,
-                      },
-                    }).then(enrollment => {
-                      moduleInfo.enrolled = enrollment !== null;
-                      return res.render(join('module', 'index'),
-                        {module: moduleInfo});
-                    });
+      .then((moduleInfo) => {
+          if (res.locals.loggedIn && res.locals.loggedIn.id === moduleInfo.authorId) {
+            moduleInfo.isMine = true;
+          }
+          return Lesson.findAll({where: {moduleId: id}})
+            .then((lessons) => {
+              moduleInfo.lessons = lessons.map(l => l.dataValues);
+              return sequelize.query(`SELECT avg(stars) AS average
+                                      from Rating AS r
+                                             INNER JOIN Module AS m
+                                      WHERE r.moduleId = m.id`)
+                .spread((avg, metadata) => {
+                  moduleInfo.rating = avg[0].average;
+                  return Rating.findAll({
+                    order: sequelize.literal('createdAt DESC'),
+                    limit: 5,
+                    where: {moduleId: moduleInfo.id},
+                  }).then(async (ratings) => {
+                    moduleInfo.ratings = ratings.map(r => r.dataValues);
+                    for (let i = 0 ; i < moduleInfo.ratings.length; i++) {
+                      moduleInfo.ratings[i].rater = (await User.findOne({where: {id: moduleInfo.ratings[i].raterId}})).dataValues;
+                    }
+                    return res.locals.loggedIn === undefined
+                      ? res.render(join('module', 'index'), {module: moduleInfo})
+                      : Enrollment.findOne({
+                        where: {
+                          moduleId: moduleInfo.id,
+                          studentId: res.locals.loggedIn.id,
+                        },
+                      }).then(enrollment => {
+                        moduleInfo.enrolled = enrollment !== null;
+                        return User.findOne({where: {id: moduleInfo.authorId}}).then(author => {
+                          moduleInfo.author = author;
+                          return res.render(join('module', 'index'), {module: moduleInfo});
+                        })
+                      });
+                  });
                 });
-              });
-          }),
+            });
+        },
       ).catch((err) => next(err));
   });
 
