c2989453a69ff9466d0012f5e752488802a2b3f9
Norbert Logiewa <norbertlogiewa96@gmail.com>
2018-12-17 00:28:13 +0000

    Extract vars into separate files

--- app.js
+++ app.js
@@ -1,80 +1,26 @@
-/**
- * Entry into the application.
- *
- * @author Norbert
- */
+require('./env')();
 
-// Standard Library
-const {join, resolve} = require('path');
-const {mkdirSync, existsSync} = require('fs');
+const {User, Session} = require('./database');
+const {decrypt} = require('./routes/lib');
+const log = require('./lib')
+  .createLogger({label: 'APP', fileLvl: 'debug', lvl: 'debug'});
 
 const SECOND = 1000;
 const MINUTE = 60 * SECOND;
-// const HOUR = 60 * MINUTE;
-
-const APP_ENV = {
-  NODE_ENV: 'development',
-  DB_SYNC: 'false',
-  MAX_RESULTS: '100',
-  PORT: '3000',
-  SECRET: 'U\x0bQ*kf\x1bb$Z\x13\x03\x15w\'- f\x0fn1\x0f\\\x106V\'M~\x07',
-  ROOT: resolve(__dirname),
-  ENCRYPTION_ALGORITHM: 'aes192',
-  SESSION_TIME: 20 * MINUTE,
-  TEST_RUNS: '20',
-  DB_PATH: join(__dirname, 'routes', 'database', 'db'),
-  LOGGING_ROUTING: 'info',
-  LOGGING_DB: 'warn',
-  LOGGING_TESTS: 'info',
-};
-
-const padStartLen = Object.keys(APP_ENV).reduce((prev, curr) => curr.length >= prev ? curr.length : prev, 0);
-const padEndLen = Object.values(APP_ENV).reduce((prev, curr) => curr.length >= prev ? curr.length : prev, 0);
-console.info(`APPLICATION ENVIRONMENT\n${'-'.repeat(padStartLen + padEndLen + 3)}`);
-for (const pair of Object.entries(APP_ENV)) {
-  const [k, v] = pair;
-  if (process.env[k] === undefined) process.env[k] = v;
-  console.info(`${k.padEnd(padStartLen)} = ${v}`);
-}
-
-/**
- * Produce a path relative to this file (i.e. path relative to the root of the project).
- *
- * @param {...String} components
- * @return {String} path relative to project root
- */
-function rootPath(...components) {
-  return resolve(join(__dirname, ...components));
-}
-
-if (!existsSync(rootPath('logs'))) mkdirSync(rootPath('logs'));
 
 const express = require('express');
 const app = express();
-const hbs = require('hbs');
-
-hbs.registerPartials(rootPath('views', 'partials'));
-
-// view engine setup
-app.set('views', rootPath('views'));
-app.set('view engine', 'hbs');
-app.engine('hbs', hbs.__express);
-app.set('x-powered-by', false);
-
-app.locals.title = 'FreeLearn';
-app.locals.authors = [
-  {name: 'Norbert Logiewa', email: 'nl253@kent.ac.uk'},
-  {name: 'Imaan Fakim', email: 'if50@kent.ac.uk'},
-  {name: 'Nicolas Valderrabano', email: 'nv55@kent.ac.uk'},
-];
-
+require('./locals')(app);
 
 app.use((req, res, next) => {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
   res.header('Access-Control-Allow-Origin', '127.0.0.1');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers',
     ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'].join(', '));
-  next();
+  return next();
 });
 
 /**
@@ -99,16 +45,14 @@ app.use(require('cors')({
   credentials: true,
 }));
 
-
 app.use(require('morgan')(':method :url :status :req[cookie]'));
-
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use(
   require('node-sass-middleware')({
-    src: rootPath('public'),
-    dest: rootPath('public'),
+    src: process.env.PUBLIC_PATH,
+    dest: process.env.PUBLIC_PATH,
     debug: false,
     outputStyle: 'compressed',
     // true = .sass and false = .scss
@@ -117,57 +61,60 @@ app.use(
   }),
 );
 
-app.use(express.static(rootPath('public')));
-
-const models = require('./routes/database');
-const {decrypt} = require('./routes/lib');
-
-app.use((req, res, next) => {
-  if (req.query) res.locals.query = req.query;
-  next();
-});
-
-
-function clearToken(res) {
-  return res.clearCookie('token', {
-    SameSite: true,
-    httpOnly: false,
-    Path: '/',
-  });
-}
+app.use(express.static(process.env.PUBLIC_PATH));
 
 app.use(async (req, res, next) => {
-  if (req.cookies.token === '') {
-    clearToken(res);
-    return next();
-  } else if (req.cookies.token === undefined) {
+  if (req.cookies.token === undefined) {
+    log.debug(`token not sent in cookies`);
     return next();
   }
   try {
-    const session = await models.Session.findOne({where: {token: decrypt(decodeURIComponent(req.cookies.token))}});
-    if (session === null) {
-      clearToken(res);
+    log.debug(`token sent in cookies`);
+    const sess = await Session.findOne({
+      where: {
+        token: decrypt(decodeURIComponent(req.cookies.token)),
+      },
+    });
+    if (sess === null && !req.originalUrl.includes('/api/')) {
+      log.debug(`token sent is does not correspond to a session`);
+      res.clearCookie('token', {
+        SameSite: 'Strict',
+        httpOnly: false,
+        Path: '/',
+      });
       return next();
-    } else if ((Date.now() - session.dataValues.updatedAt) >= (process.env.SESSION_TIME || 20 * MINUTE)) {
-      await session.destroy();
-      clearToken(res);
+    } else if ((Date.now() - sess.updatedAt) >=
+      (process.env.SESSION_TIME || 20 * MINUTE)) {
+      log.debug(`token sent is stale, destroying associated session`);
+      sess.destroy();
+      if (!req.originalUrl.includes('/api/')) {
+        res.clearCookie('token', {
+          SameSite: 'Strict',
+          httpOnly: false,
+          Path: '/',
+        });
+      }
       return next();
     } else {
-      await session.update({updatedAt: Date.now()});
+      log.debug(`valid token sent, refreshing it`);
+      await sess.update({updatedAt: Date.now()});
     }
-    res.locals.loggedIn = (await models.User.findOne({
-      where: {email: session.email},
-      attributes: Object.keys(models.User.attributes).filter(a => a !== 'password'),
-    })).dataValues;
+    res.locals.loggedIn = await User.findOne({
+      where: {email: sess.email},
+      attributes: {exclude: ['password']},
+    }).then(u => u.dataValues);
+    log.info(`logged in: ${Object.entries(res.locals.loggedIn)
+      .map(pair => pair.join(' = '))
+      .join(', ')}`);
     return next();
-  } catch(err) {
-    return console.error(err);
-  } 
+  } catch (err) {
+    return next(err);
+  }
 });
 
-app.use('/api', require('./routes/api'));
 app.use('/user', require('./routes/user'));
 app.use('/module', require('./routes/module'));
+app.use('/api', require('./routes/api'));
 app.use('/', require('./routes'));
 
 // catch 404 and forward to error handler
@@ -175,12 +122,10 @@ app.use((req, res, next) => next(require('http-errors')(404)));
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV === 'development' ? err : {};
-
-  // render the error page
-  return res.status(err.status || err.code || 500).render('error');
+  log.error(err);
+  return res
+    .status(err.status || err.code || 500)
+    .render('error', {message: err.message, error: err});
 });
 
 module.exports = app;
